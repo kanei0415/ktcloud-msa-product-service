5d124e4b1be0bf4c20a92eb45c643411a9887213
K4NEI <kimyoungho0415@gmail.com>
2026-05-09 07:39:55 +0900

    Split modules [product-service product] to ktcloud-msa-product-service

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,18 +18,6 @@ dependencyResolutionManagement {
 rootProject.name = "market"
 
 include(
-    "inventory",
-    "inventory-service",
-    "order",
-    "order-service",
     "product",
     "product-service",
-    "common",
-    "client-redis",
-    "inventory-event",
-    "user",
-    "identification",
-    "auth",
-    "client-ses",
-    "user-api-gateway",
 )
