e1144223c1a07c458b402f3d192ea7a0619e3bdf
K4NEI <kimyoungho0415@gmail.com>
2026-05-09 07:30:02 +0900

    Split modules [product-service product] to ktcloud-msa-product-service

--- product/build.gradle.kts
+++ product/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    implementation("https://maven.pkg.github.com/kanei0415/ktcloud-market-msa-common:1.0.0")
+    implementation("com.kanei0415:ktcloud-market-msa-common:1.0.0")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
 
