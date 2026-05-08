f8e01aacdd1959b786a4c39605242bb906ae9d4c
K4NEI <kimyoungho0415@gmail.com>
2026-05-08 19:47:39 +0900

    Split modules [product-service product] to ktcloud-msa-product-service

---
+++ product/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.jpa")
+}
+
+dependencies {
+    implementation("https://maven.pkg.github.com/kanei0415/ktcloud-market-msa-common:1.0.0")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    runtimeOnly("com.h2database:h2")
+}
