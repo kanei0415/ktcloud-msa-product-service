2df9686b669566db890782e52848ec1a00019891
K4NEI <kimyoungho0415@gmail.com>
2026-05-10 13:52:07 +0900

    .

--- product/bin/main/dev/ktcloud/black/product/domain/entity/ProductDomainEntity.kt
+++ product/bin/main/dev/ktcloud/black/product/domain/entity/ProductDomainEntity.kt
@@ -1,7 +1,9 @@
 package dev.ktcloud.black.product.domain.entity
 
+import java.util.UUID
+
 data class ProductDomainEntity(
-    val id: String = "",
+    val id: UUID = UUID.randomUUID(),
     val name: String,
     val description: String,
     val price: Int,
